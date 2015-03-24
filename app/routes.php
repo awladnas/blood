ddb58c6e1819e40b544c88f47ea0dbf53038ef28
awladnas <awlad@nascenia.com>
2015-03-24 16:34:28 +0600

    fixed token expiration issue and added password change and update token functionality

--- app/routes.php
+++ app/routes.php
@@ -36,6 +36,8 @@ Route::group(array('prefix' => 'admin'), function(){
 Route::group(array('prefix' => 'api/v1'),function(){
     Route::post('users/create',array('as' => 'users.create.path', 'uses' => 'LifeLi\controllers\UsersController@store'));
     Route::group(['before' => 'auth.validate_token'], function(){
+        Route::post('users/{id}/change_password', 'LifeLi\controllers\UsersController@change_password');
+        Route::get('users/{id}/update_token', 'LifeLi\controllers\UsersController@update_token');
         Route::resource('users', 'LifeLi\controllers\UsersController');
         Route::group(['prefix' => 'users'], function(){
             Route::get('search/{profile_id}',['uses' => 'LifeLi\controllers\ProfilesController@search_user']);
