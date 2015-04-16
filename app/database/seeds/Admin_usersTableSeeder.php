44359931a3cb80fda7307446e7d418179741bc7b
awladnas <awlad@nascenia.com>
2015-04-16 11:29:22 +0600

    added offers to admin panel

--- app/database/seeds/Admin_usersTableSeeder.php
+++ app/database/seeds/Admin_usersTableSeeder.php
@@ -5,14 +5,17 @@ class Admin_usersTableSeeder extends Seeder {
 	public function run()
 	{
 		// Uncomment the below to wipe the table clean before populating
-		// DB::table('admin_users')->truncate();
+		DB::table('admin_users')->truncate();
 
-		$admin_users = array(
-
-		);
+        $arrData = [
+            'name' => 'admin',
+            'email' => 'admin@lifeli.com',
+            'password' => Hash::make('admin123'),
+            'role' => 'super_admin'
+        ];
 
 		// Uncomment the below to run the seeder
-		// DB::table('admin_users')->insert($admin_users);
+		 DB::table('admin_users')->insert($arrData);
 	}
 
 }
